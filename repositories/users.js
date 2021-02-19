e6f75cec010b259f8e3784a3af70472225fb1d7b
TonyYFL <litony179@gmail.com>
2021-02-19 23:12:50 +1100

    users.js update

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
+
+const scrypt = util.promisify(crypto.scrypt);
+
 class usersRepository {
     constructor(filename) {
         if (!filename) {
@@ -20,11 +24,34 @@ class usersRepository {
     }
 
     async create(attributes) {
+        // {email: '', password: ''}
+
         attributes.id = this.randomID();
+        const salt = crypto.randomBytes(8).toString('hex');
+
+        const buf = await scrypt(attributes.password, salt, 64);
+
         const records = await this.getAll();
-        records.push(attributes);
+        const record = {
+            ...attributes,
+            password: `${buf.toString('hex')}.${salt}`
+        };
+
+        records.push(record);
         await this.writeAll(records);
-        return attributes;
+        return record;
+    }
+
+    async comparePassword(saved, supplied) {
+        //saved = password saved in our database
+        //supplied = password supplied by the client
+        const result = saved.split('.');
+        const hashed = result[0];
+        const salt = result[1];
+        const hashedSupplied = await scrypt(supplied, salt, 64);
+
+        return hashed === hashedSupplied;
+
     }
 
     async writeAll(records) {
